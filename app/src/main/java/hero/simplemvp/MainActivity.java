ddf0331356be06c474a52a19fe0540d8b07e7d6c
appzy <15523961070@163.com>
2017-06-23 21:41:51 +0800

    class description function description

--- app/src/main/java/hero/simplemvp/MainActivity.java
+++ app/src/main/java/hero/simplemvp/MainActivity.java
@@ -4,11 +4,24 @@ import android.os.Bundle;
 
 import hero.simplemvp.base.BaseActivity;
 
+/**
+ * @date 创建时间：2017/6/23
+ * @author appzy
+ * @Description 风吹过城市
+ * @version 1.0
+ */
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    /**
+     * @date 创建时间：2017/6/23
+     * @author appzy
+     * @Description 啊哈哈
+     * @version
+     */
 }
